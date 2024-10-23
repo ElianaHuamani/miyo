0a3bb89ba727857e0366df6492e97464dcec2042
Eliana Huamani <eliana.huamani@tektonlabs.com>
2024-10-22 21:05:42 -0500

    fix images mock

--- src/mocks/DiscoveryCardsMock.ts
+++ src/mocks/DiscoveryCardsMock.ts
@@ -1,21 +1,25 @@
 // Datos simulados (mock) para las tarjetas de la sección
+import journeyfinanzaspersonales from '@/assets/images/journey-finanzas-personales.svg';
+import journeyinveriones from '@/assets/images/journey-inveriones.svg';
+import journeyplanificacionfinanciera from '@/assets/images/journey-planificacion-financiera.svg';
+
 export const section1CardsMock = [
     { 
       title: 'Finanzas personales', 
       description: 'TEST -Esta es una descripción que no supera los 50 caracteres.', 
-      image: '/src/assets/images/journey-finanzas-personales.svg', 
+      image: journeyfinanzaspersonales, 
       link: '/pagina1'
     },
     { 
       title: 'Inversiones', 
       description: 'Descripción un poco más larga que debería truncarse a los 50 caracteres para no mostrar demasiado contenido.', 
-      image: '/src/assets/images/journey-inveriones.svg', 
+      image: journeyinveriones, 
       link: '/pagina2'
     },
     { 
       title: 'Planificacion Financiera', 
       description: 'Descripción corta.', 
-      image: '/src/assets/images/journey-planificacion-financiera.svg', 
+      image: journeyplanificacionfinanciera, 
       link: '/pagina3'
     }
   ];
